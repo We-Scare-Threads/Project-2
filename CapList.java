7d95ffbcac65ae681e34f0170ce0a6b19448b0df
Deonicles <djrious3@gmail.com>
2025-10-25 18:06:49 -0500

    Add files via upload

--- CapList.java
+++ CapList.java
@@ -1,4 +1,4 @@
-//Task 3: Implementation with Capability Lists for Domains
+//Task 3 - Updated with Users_CList
 
 import java.util.LinkedList;
 import java.util.Random;
@@ -7,33 +7,38 @@ import java.util.concurrent.locks.ReentrantLock;
 
 class CapList {
     static Lock[] resources;
+    static String[] data;
+    public final static String[] Colors = {"blue", "red", "green", "yellow", "purple", "orange", "pink", "brown", "black", "white"};
+
 
     public static void main(String[] args) {
-        System.out.println("\nCapacity List Key: ");
-        System.out.println("R = Read, W = Write, B = Both (Read/Write), E = Empty/No Access, N = No Access (for Domains ONLY)");
+        System.out.println("\nCapability List Key: ");
+        System.out.println("R = Read, W = Write, B = Both (Read/Write), E = Empty/No Access, A = Allow Domain Switch");
         Random random = new Random();
 
         int numDomains = random.nextInt(5) + 3;
         int numObjects = random.nextInt(5) + 3;
 
         System.out.println("D: " + numDomains);
-        System.out.println("O: " + numObjects + "\n");
+        System.out.println("F: " + numObjects + "\n");
 
         LinkedList<LinkedList<String>> capList = CList(numDomains, numObjects);
 
         resources = new Lock[numObjects + numDomains];
+        data = new String[numObjects + numDomains];
         for (int r = 0; r < (numObjects + numDomains); r++) {
             resources[r] = new ReentrantLock();
+            data[r] = Colors[random.nextInt(Colors.length)];
         }
         System.out.println("Resources Array holds: " + numObjects + " files(objects) and " + numDomains + " domains. " + resources.length + " in total.\n");
         System.out.println("Num of Users (domains) shall be: " + numDomains + ".\n");
         System.out.println("User Access Key: ");
-        System.out.println("(O) = Accessed Object, (R) = Read, (W) = Write, (E) = No Assigned Permissions, **An attached X = Access Denied**\n");
+        System.out.println("(F) = Accessed File, (R) = Read, (W) = Write, (A) = Allow Domain Switch, **An attached X = Access Denied**\n");
 
-        //for (int i = 0; i < numDomains; i++){
-        //    Users user = new Users(i, aListD, aListF, resources, numDomains, numObjects);
-        //    user.start();
-        //}
+        for (int i = 0; i < numDomains; i++) {
+            Users_CList user = new Users_CList(i, capList, resources, numDomains, numObjects, data, Colors);
+            user.start();
+        }
     }
 
     public static LinkedList<LinkedList<String>> CList(int n, int m) {
@@ -45,25 +50,54 @@ class CapList {
 
         LinkedList<LinkedList<String>> lists = new LinkedList<>();
 
-        for (int i = 1; i <= n + m; i++) {
+        for (int i = 0; i < n; i++) {
             LinkedList<String> cList = new LinkedList<>();
             lists.add(cList);
         }
 
-        System.out.println("Capacity List:");
-        for(int j=1; j<=n; j++) {
-            for (int i = 1; i <= m; i++) {
+        System.out.println("Capability List:");
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 r = random.nextInt(4);
-                lists.get(j).add(objperms[r]);
+                lists.get(i).add(objperms[r]);
             }
-            for (int i = 1; i <= n; i++) {
-                r = random.nextInt(2);
-                lists.get(j).add(domperms[r]);
+            for (int k = 0; k < n; k++) {
+                if (k == i) {
+                    lists.get(i).add("E");
+                } else {
+                    r = random.nextInt(2);
+                    lists.get(i).add(domperms[r]);
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("D" + i + " --> ");
+            boolean first_entry = true;
+
+            for (int j = 0; j < m; j++) {
+                String perms = lists.get(i).get(j);
+                if (!perms.equals("E") && (!perms.equals("N"))) {
+                    if (!first_entry) System.out.print(" -> ");
+                    System.out.print("F" + j + ":" + perms);
+                    first_entry = false;
+                }
             }
 
-        }
-        for (int i = 1; i <= n; i++) {
-            System.out.println("D" + i + " --> "+lists.get(i));
+            for (int k = 0; k < n; k++) {
+                String perm = lists.get(i).get(m + k);
+                if (!perm.equals("E") && (!perm.equals("N"))) {
+                    if (!first_entry) System.out.print(" -> ");
+                    System.out.print("D" + k + ":" + perm);
+                    first_entry = false;
+                }
+            }
+
+            if (first_entry) {
+                System.out.print("No permissions");
+            }
+            System.out.println();
         }
 
         return lists;
---
+++ Users_CList.java
@@ -0,0 +1,120 @@
+import java.util.LinkedList;
+import java.util.Random;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class Users_CList extends Thread {
+    private int userId;
+    Lock[] resources;
+    private final LinkedList<LinkedList<String>> capList;
+    static String[] objActions = {"R", "W"};
+    static String domAction = "A";
+    private int numDomains;
+    private int numObjects;
+    private String[] data;
+    private static String[] Colors;
+
+    public Users_CList(int userId, LinkedList<LinkedList<String>> capList, Lock[] resources, int numDomains, int numObjects, String[] data, String[] Colors) {
+        this.userId = userId;
+        this.capList = capList;
+        this.resources = resources;
+        this.numDomains = numDomains;
+        this.numObjects = numObjects;
+        this.data = data;
+        this.Colors = Colors;
+    }
+
+    public void yieldR() {
+        Random random = new Random();
+        int y = random.nextInt(5) + 3;
+        System.out.println("User D" + userId + " yields " + y + " times");
+        for (int i = 0; i < y; i++) {
+            Thread.yield();
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            Random random = new Random();
+            int requestCount = 0;
+
+            while (requestCount < 5) {
+                int objId = random.nextInt(resources.length);
+                int actionIndex = random.nextInt(objActions.length);
+
+                if (objId >= numObjects && (objId - numObjects) == userId) {
+                    continue;
+                }
+
+                String permission = capList.get(userId).get(objId);
+                String action;
+
+                if (objId >= numObjects) {
+                    if (permission.equals("A")) {
+                        resources[objId].lock();
+                        System.out.println("User D" + userId + " SWITCHING to Domain D" + (objId - numObjects) + " (A)");
+                        yieldR();
+                        switchUser(objId);
+                        resources[objId].unlock();
+                    } else {
+                        resources[objId].lock();
+                        System.out.println("User D" + userId + " tried switch with NO permissions on Domain D" + (objId - numObjects) + " (" + permission + ") - X");
+                        yieldR();
+                        resources[objId].unlock();
+                    }
+                } else {
+                    action = objActions[actionIndex];
+                    switch (action) {
+                        case "R":
+                            if (permission.equals("E")) {
+                                resources[objId].lock();
+                                System.out.println("User D" + userId + " lacks permissions for READ on (O" + objId + ") (E) - X");
+                                yieldR();
+                                resources[objId].unlock();
+                            } else if (permission.equals("R") || permission.equals("B")) {
+                                resources[objId].lock();
+                                System.out.println("User D" + userId + " accessed (O" + objId + ") Reading '" + data[objId] + "' with (R)");
+                                yieldR();
+                                resources[objId].unlock();
+                            } else {
+                                resources[objId].lock();
+                                System.out.println("User D" + userId + " CANNOT READ (F" + objId + ") with (" + permission + ") - X");
+                                yieldR();
+                                resources[objId].unlock();
+                            }
+                            break;
+                        case "W":
+                            if (permission.equals("E")) {
+                                resources[objId].lock();
+                                System.out.println("User D" + userId + " lacks permission for WRITE on (O" + objId + ") (E) - X");
+                                yieldR();
+                                resources[objId].unlock();
+                            } else if (permission.equals("W") || permission.equals("B")) {
+                                resources[objId].lock();
+                                int r = random.nextInt(Colors.length);
+                                data[objId] = Colors[r];
+                                System.out.println("User D" + userId + " accessed (O" + objId + ") Writing '" + data[objId] + "' with (W)");
+                                yieldR();
+                                resources[objId].unlock();
+                            } else {
+                                resources[objId].lock();
+                                System.out.println("User D" + userId + " CANNOT WRITE (F" + objId + ") with (" + permission + ") - X");
+                                yieldR();
+                                resources[objId].unlock();
+                            }
+                            break;
+                    }
+                }
+                requestCount++;
+            }
+        } catch (Error e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public void switchUser(int objId) {
+        userId = (objId - numObjects);
+        System.out.println("User domain changed to D" + userId);
+    }
+}
